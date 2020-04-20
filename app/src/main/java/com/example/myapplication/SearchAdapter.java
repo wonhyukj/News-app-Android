253838b55283d7d1ea9bd263717e9160a003a93e
Wonhyuk Jang <wonhyuk12@gmail.com>
2020-04-20 16:04:36 -0700

    hw

---
+++ app/src/main/java/com/example/myapplication/SearchAdapter.java
@@ -0,0 +1,238 @@
+
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class SearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final Context context;
+    private LayoutInflater inflater;
+    private List<News> news;
+
+    SearchAdapter(Context ctx, List<News> news) {
+        this.context = ctx;
+        this.inflater = LayoutInflater.from(ctx);
+        this.news = news;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        RecyclerView.ViewHolder viewHolder;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_list_layout, parent, false);
+        viewHolder = new NewsViewHolder(view);
+        return viewHolder;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        NewsViewHolder holders = (NewsViewHolder) holder;
+
+        News news = (News) this.news.get(position);
+        ZonedDateTime timeNow = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        ZonedDateTime timeGenerated = ZonedDateTime.parse(news.getTime());
+        timeGenerated = timeGenerated.withZoneSameLocal(ZoneId.of("America/Los_Angeles"));
+
+        Duration d = Duration.between(timeNow, timeGenerated);
+        long seconds = d.getSeconds();
+        long absSeconds = Math.abs(seconds);
+        String timeReturn = "";
+        if (absSeconds / 3600 > 0) {
+            timeReturn = (absSeconds / 3600) + "h ago";
+        } else if ((absSeconds % 3600) / 60 > 0) {
+            timeReturn = ((absSeconds % 3600) / 60) + "m ago";
+        } else {
+            timeReturn = (absSeconds % 60) + "s ago";
+        }
+
+        holders.newsTitle.setText(news.getTitle());
+        holders.newsSource.setText(news.getSection());
+        holders.newsDate.setText(timeReturn);
+        holders.newsTime.setText(news.getTime());
+        holders.newsURL.setText(news.getWebURL());
+        holders.newsID.setText(news.getId());
+        holders.newsImgURL.setText(news.getImg());
+        if (news.getImg().equals("Guardians_hq.png")) {
+            Picasso.get().load(R.drawable.default_img).fit().into(holders.newsImage);
+        } else {
+            Picasso.get().load(news.getImg()).fit().into(holders.newsImage);
+        }
+        //BookMark
+        if (SharedPreference.getSavedObjectFromPreference(context, "storage", holders.newsID.getText().toString(), News.class) == null) {
+            holders.newsBookmark.setTag("noBookmark");
+        } else {
+            holders.newsBookmark.setTag("Bookmark");
+            holders.newsBookmark.setImageResource(R.drawable.ic_bookmark_24px);
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return this.news.size();
+    }
+
+    public static class NewsViewHolder extends RecyclerView.ViewHolder {
+        TextView newsTitle, newsSource, newsDate, newsID, newsURL, newsImgURL, newsTime;
+
+        ImageView newsImage, newsBookmark, newsTwitter, dialog_bookmark;
+
+        Intent intent;
+
+        NewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            newsTime = itemView.findViewById(R.id.newsTime);
+            newsImage = itemView.findViewById(R.id.newsImage);
+            newsTitle = itemView.findViewById(R.id.newsTitle);
+            newsSource = itemView.findViewById(R.id.newsSource);
+            newsDate = itemView.findViewById(R.id.newsDate);
+            newsBookmark = itemView.findViewById(R.id.bookmark);
+            newsTwitter = itemView.findViewById(R.id.twitter);
+            newsURL = itemView.findViewById(R.id.newsURL);
+            newsID = itemView.findViewById(R.id.newsID);
+            dialog_bookmark = itemView.findViewById(R.id.dialog_bookmark);
+            newsImgURL = itemView.findViewById(R.id.newsImgURL);
+
+            newsBookmark.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("CONTEXT: ", view.getContext().toString());
+                    if (newsBookmark.getTag().equals("noBookmark")) {
+                        newsBookmark.setImageResource(R.drawable.ic_bookmark_24px);
+                        newsBookmark.setTag("Bookmark");
+                        Log.i("NEWS ID: ", newsID.getText().toString());
+                        Toast.makeText(view.getContext(), "ID" + newsID.getText().toString(), Toast.LENGTH_LONG).show();
+                        News news = new News();
+
+                        news.setId(newsID.getText().toString());
+                        news.setImg(newsImgURL.getText().toString());
+                        news.setSection(newsSource.getText().toString());
+                        news.setTime(newsTime.getText().toString());
+                        news.setTitle(newsTitle.getText().toString());
+                        news.setWebURL(newsURL.getText().toString());
+                        SharedPreference.saveObjectToSharedPreference(view.getContext(), "storage", news.getId(), news);
+                        Log.i("LOG ID: ", "ID" + SharedPreference.getSavedObjectFromPreference(view.getContext(), "storage", newsID.getText().toString(), News.class));
+
+                    } else {
+                        newsBookmark.setImageResource(R.drawable.ic_bookmark_border_24px);
+                        newsBookmark.setTag("noBookmark");
+                        SharedPreference.removeSavedObjectFromPreference(view.getContext(), "storage", newsID.getText().toString());
+//                        HomeAdapter.context = view.getContext();
+                        Log.i("LOG ID: ", "ID" + SharedPreference.getSavedObjectFromPreference(view.getContext(), "storage", newsID.getText().toString(), News.class));
+                    }
+                }
+            });
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    intent = new Intent(v.getContext(), ArticleActivity.class);
+                    intent.putExtra("url", newsID.getText());
+                    intent.putExtra("newsImage", newsImgURL.getText().toString());
+                    intent.putExtra("newsSource", newsSource.getText());
+                    intent.putExtra("newsDate", newsDate.getText());
+                    intent.putExtra("newsTime", newsTime.getText());
+                    intent.putExtra("newsTitle", newsTitle.getText());
+                    intent.putExtra("newsURL", newsURL.getText());
+                    v.getContext().startActivity(intent);
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    final Dialog dialog = new Dialog(view.getContext());
+                    // Include dialog.xml file
+                    dialog.setContentView(R.layout.dialog);
+                    // Set dialog title
+                    dialog.setTitle("Custom Dialog");
+
+                    // set values for custom dialog components - text, image and button
+                    TextView text = dialog.findViewById(R.id.textDialog);
+                    text.setText(newsTitle.getText());
+                    ImageView image = dialog.findViewById(R.id.imageDialog);
+
+                    Bitmap bitmap = ((BitmapDrawable) newsImage.getDrawable()).getBitmap();
+
+                    image.setImageBitmap(bitmap);
+
+                    ImageView twitter = dialog.findViewById(R.id.twitter);
+                    twitter.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/intent/tweet?text=Check out this Link: &url="
+                                    + newsURL.getText() + "&hashtags=CSCI571NewsSearch"));
+                            view.getContext().startActivity(intent);
+                        }
+                    });
+
+                    final ImageView dialog_Bookmark = dialog.findViewById(R.id.dialog_bookmark);
+
+                    if (SharedPreference.getSavedObjectFromPreference(view.getContext(), "storage", newsID.getText().toString(), News.class) == null) {
+                        dialog_Bookmark.setImageResource(R.drawable.ic_bookmark_border_24px);
+                    } else {
+                        dialog_Bookmark.setImageResource(R.drawable.ic_bookmark_24px);
+                    }
+
+                    dialog_Bookmark.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if (newsBookmark.getTag().equals("noBookmark")) {
+                                News news = new News();
+
+                                news.setId(newsID.getText().toString());
+                                news.setImg(newsImgURL.getText().toString());
+                                news.setSection(newsSource.getText().toString());
+                                news.setTime(newsTime.getText().toString());
+                                news.setTitle(newsTitle.getText().toString());
+                                news.setWebURL(newsURL.getText().toString());
+
+                                dialog_Bookmark.setImageResource(R.drawable.ic_bookmark_24px);
+                                newsBookmark.setImageResource(R.drawable.ic_bookmark_24px);
+                                SharedPreference.saveObjectToSharedPreference(view.getContext(), "storage", newsID.getText().toString(), news);
+                                newsBookmark.setTag("Bookmark");
+
+                            } else {
+                                dialog_Bookmark.setImageResource(R.drawable.ic_bookmark_border_24px);
+                                newsBookmark.setImageResource(R.drawable.ic_bookmark_border_24px);
+                                SharedPreference.removeSavedObjectFromPreference(view.getContext(), "storage", newsID.getText().toString());
+                                newsBookmark.setTag("noBookmark");
+                            }
+                        }
+                    });
+                    dialog.show();
+
+                    return false;
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/SearchResultsActivity.java
+++ app/src/main/java/com/example/myapplication/SearchResultsActivity.java
@@ -1,29 +1,274 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-public class SearchResultsActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+public class SearchResultsActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
     private String query;
+    TextView searchTerm;
+    private List<News> news;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ProgressBar progressBar;
+    private TextView progressBarTextView;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+        news = new ArrayList<>();
+
         query = getIntent().getStringExtra(SearchManager.QUERY);
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.search_actionbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        searchTerm = findViewById(R.id.searchKeyword);
+        searchTerm.setText(query);
+
+        progressBar = findViewById(R.id.home_progress);
+        progressBarTextView = findViewById(R.id.home_textview);
+
+        swipeRefreshLayout = findViewById(R.id.swipe_container);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBarTextView.setVisibility(View.VISIBLE);
+        extractNews(query);
+        recyclerView = findViewById(R.id.recycler_search);
+        SearchAdapter searchAdapter = new SearchAdapter(getApplicationContext(), news);
+        recyclerView.setAdapter(searchAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Objects.requireNonNull(this.recyclerView.getAdapter()).notifyDataSetChanged();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+
         return super.onCreateView(name, context, attrs);
+
     }
 
 
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                extractNews(query);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, 1500);
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+
+    private void extractNews(String query) {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String JSON_URL = "http://nodeserverandroid-env.eba-cxvrpe5n.us-west-2.elasticbeanstalk.com/Search?id=" + query;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("temp");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        try {
+                            JSONObject newsObject = jsonArray.getJSONObject(i);
+                            Log.i("JSON OBJ ", newsObject.toString());
+                            News newObj = new News();
+                            newObj.setId(newsObject.getString("id"));
+                            newObj.setTitle(newsObject.getString("titles"));
+                            newObj.setTime(newsObject.getString("date"));
+                            newObj.setSection(newsObject.getString("section"));
+                            newObj.setWebURL(newsObject.getString("direct"));
+                            newObj.setImg(newsObject.getString("img"));
+                            newObj.setNewsImgURL(newsObject.getString("img"));
+                            news.add(newObj);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    recyclerView.getAdapter().notifyDataSetChanged();
+                    progressBar.setVisibility(View.GONE);
+                    progressBarTextView.setVisibility(View.GONE);
+                    Objects.requireNonNull(recyclerView.getAdapter()).notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("tag", "onErrorResponse: " + error.getMessage());
+            }
+        });
+        queue.add(jsonObjectRequest);
+    }
+
 }
+
+
+//public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+//    private List<News> news;
+//    private RecyclerView recyclerView;
+//    private SwipeRefreshLayout swipeRefreshLayout;
+//    private ProgressBar progressBar;
+//    private TextView progressBarTextView;
+//    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        news = new ArrayList<>();
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        Objects.requireNonNull(this.recyclerView.getAdapter()).notifyDataSetChanged();
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.home_layout, container, false);
+//        recyclerView = root.findViewById(R.id.recycler_view);
+//
+//        SearchAdapter searchAdapter = new SearchAdapter(getApplicationContext(), news);
+//        recyclerView.setAdapter(searchAdapter);
+//        progressBar = root.findViewById(R.id.home_progress);
+//        progressBarTextView = root.findViewById(R.id.home_textview);
+//
+//        swipeRefreshLayout = root.findViewById(R.id.swipe_container);
+//        swipeRefreshLayout.setOnRefreshListener(this);
+//
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+//        recyclerView.setLayoutManager(layoutManager);
+//        progressBar.setVisibility(View.VISIBLE);
+//        progressBarTextView.setVisibility(View.VISIBLE);
+//        extractNews(query);
+//        return root;
+//    }
+//
+//    private Activity getMainActivity() {
+//        return Objects.requireNonNull(getActivity());
+//    }
+//
+//    private Context getApplicationContext() {
+//        return getMainActivity().getApplicationContext();
+//    }
+//
+//    private void extractNews(String query) {
+//        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+//        String JSON_URL = "http://nodeserverandroid-env.eba-cxvrpe5n.us-west-2.elasticbeanstalk.com/Search?id=" + query;
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                try {
+//                    JSONArray jsonArray = response.getJSONArray("temp");
+//                    for (int i = 0; i < jsonArray.length(); i++) {
+//                        try {
+//                            JSONObject newsObject = jsonArray.getJSONObject(i);
+//                            Log.i("JSON OBJ ", newsObject.toString());
+//                            News newObj = new News();
+//                            newObj.setId(newsObject.getString("id"));
+//                            newObj.setTitle(newsObject.getString("webTitle"));
+//                            newObj.setTime(newsObject.getString("webPublicationDate"));
+//                            newObj.setSection(newsObject.getString("section"));
+//                            newObj.setWebURL(newsObject.getString("webUrl"));
+//                            newObj.setImg(newsObject.getString("bigUrl"));
+//                            newObj.setNewsImgURL(newsObject.getString("bigUrl"));
+//                            news.add(newObj);
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                    progressBar.setVisibility(View.GONE);
+//                    progressBarTextView.setVisibility(View.GONE);
+//                    Objects.requireNonNull(recyclerView.getAdapter()).notifyDataSetChanged();
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.d("tag", "onErrorResponse: " + error.getMessage());
+//            }
+//        });
+//        queue.add(jsonObjectRequest);
+//    }
+//
+//
+//    @Override
+//    public void onRefresh() {
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                extractNews(query);
+//                swipeRefreshLayout.setRefreshing(false);
+//            }
+//        }, 1500);
+//    }
+//}
+//
